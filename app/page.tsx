42224b03d5a45504a84ca226dd3a3dbc95415546
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-03-08 21:50:45 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero1 from "@/components/accueil/Hero1";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background font-sans">
+    <div className="min-h-screen bg-slate-200 font-sans">
       
       <main className="flex flex-col">
         <Hero1 />
