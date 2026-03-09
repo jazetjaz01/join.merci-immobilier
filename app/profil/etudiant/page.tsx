4a449cd56e953ee9e63c1be428726f81886152a4
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-03-09 22:23:33 +0100

    update

---
+++ app/profil/etudiant/page.tsx
@@ -0,0 +1,18 @@
+import Hero1 from "@/components/profil/etudiant/Hero1";
+
+
+
+
+export default function Etudiant() {
+  return (
+    <div className="min-h-screen bg-slate-200 font-sans">
+      
+      <main className="flex flex-col">
+        <Hero1 />
+      
+
+       
+      </main>
+    </div>
+  );
+}
